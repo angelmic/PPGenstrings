7b3cbb7380758d11cd9d27a2382abf75664a7a33
richard <lichang.liu@gmail.com>
2017-03-02 12:53:22 +0800

    init

---
+++ PPGenstrings/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PPGenstrings
+//
+//  Created by richard on 2017/3/2.
+//  Copyright © 2017年 richard. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
